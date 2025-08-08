59a2a7e2010492f3fe2ce5d52e36182ff94e29b8
BhavinPadiya <143876932+BhavinPadiya@users.noreply.github.com>
2025-08-08 16:30:06 +0530

    added game logic

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ function getComputerChoice() {
     const choice= ['Rock','Paper','Scissors'];
     return choice[Math.floor(Math.random()*3)];
 }
+
+
+// Take player choice
+let playerchoice = prompt("Rock, Paper or Scissors? ").toLowerCase();
+
+
+// Play logic
+function playRound(playerselection, computerselection) {
+    if(playerselection === computerselection){
+        return "It's a tie!";
+    }
+
+    if(
+        (playerselection === "rock" && computerselection === "scissors") ||
+        (playerselection === "paper" && computerselection === "rock") ||
+        (playerselection === "scissors" && computerselection === "paper")
+    ){
+        return `You win! ${playerselection} beats ${computerselection}`;
+    }else{
+        return `You lose! ${computerselection} beats ${playerselection}`;
+    }
+}
+
+
+//Play
+const computerchoice = getComputerChoice().toLowerCase();
+console.log(playRound(playerchoice, computerchoice));
